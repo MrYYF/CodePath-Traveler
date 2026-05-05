d740f7d4bb4761d8d5d89e344f974ff2f212e59c
YYF <1760945581@qq.com>
2026-05-05 17:58:24 +0800

    【装备UI】备选装备面板

---
+++ Assets/Scripts/UI/Equipment/EquipmentCharacterTabButton.cs
@@ -0,0 +1,54 @@
+
+
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EquipmentCharacterTabButton : MonoBehaviour, ISelectHandler {
+    [SerializeField] private Image portraitImage;
+    [SerializeField] private CanvasGroup canvasGroup;
+
+    private Button _button;
+
+    private int _index;
+
+    private Action<int> _onTabSelected;
+    private Action<int> _onTabClicked;
+
+    public Button Button => _button;
+
+    private void Awake() {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(HandleClicked);
+        SetSelectedVisual(false);
+    }
+
+    public void Setup(CharacterRuntimeData member, int index, Action<int> onTabSelected, Action<int> onTabClicked) {
+        _index = index;
+        _onTabSelected = onTabSelected;
+        _onTabClicked = onTabClicked;
+
+        if (portraitImage == null) return;
+
+        portraitImage.sprite = member.Definition.Portrait;
+    }
+
+    public void SetSelectedVisual(bool isSelected) {
+        canvasGroup.alpha = isSelected ? 1f : 0.5f;
+    }
+
+    public void SetInteractable(bool isInteractable) {
+        _button.interactable = isInteractable;
+    }
+
+    private void HandleClicked() {
+        if (_button == null || !_button.interactable) return;
+        _onTabClicked?.Invoke(_index);
+    }
+
+    public void OnSelect(BaseEventData eventData) {
+        if (_button == null || !_button.interactable) return;
+
+        _onTabSelected?.Invoke(_index);
+    }
+}
---
+++ Assets/Scripts/UI/Equipment/EquipmentStatCompareRow.cs
@@ -0,0 +1,30 @@
+using TMPro;
+
+public class EquipmentStatCompareRow : MonoBehaviour {
+    [SerializeField] private TMP_Text currentValueText;
+    [SerializeField] private TMP_Text modifyValueText;
+
+    [Header("Color Settings")]
+    [SerializeField] private Color positiveColor = Color.green;
+    [SerializeField] private Color negativeColor = Color.red;
+
+
+    public void SetRow(int currentValue, int previewValue, bool isInPreviewMode) {
+        currentValueText.text = currentValue.ToString();
+
+        if(!isInPreviewMode || previewValue == currentValue) {
+            modifyValueText.text = "";
+            return;
+        }
+
+        modifyValueText.text = previewValue.ToString();
+
+        if(previewValue > currentValue) {
+            modifyValueText.color = positiveColor;
+        } else if (previewValue < currentValue) {
+            modifyValueText.color = negativeColor;
+        } else {
+            modifyValueText.color = Color.white; // No change
+        }
+    }
+}
