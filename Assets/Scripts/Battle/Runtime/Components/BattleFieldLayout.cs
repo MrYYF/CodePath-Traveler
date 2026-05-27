7394707d0c6fad385ce4300a1bd297aa17a9ddcb
YYF <1760945581@qq.com>
2026-05-27 10:21:52 +0800

    【战斗系统】设置场景站位布局

---
+++ Assets/Scripts/Battle/Runtime/Components/BattleFieldLayout.cs
@@ -0,0 +1,45 @@
+
+
+public class BattleFieldLayout : MonoBehaviour {
+    [Header("Scene Reference")]
+    public Transform actionTrans;
+    public Transform initTrans;
+    [Header("Ally Slot Segment")]
+    [SerializeField] private Transform allyTopTrans;
+    [SerializeField] private Transform allyBottomTrans;
+    [Header("Enemy Slot Segment(Normal)")]
+    [SerializeField] private Transform enemyTopTrans;
+    [SerializeField] private Transform enemyBottomTrans;
+    [Header("Enemy Slot Segment(Boss Triangle)")]
+    [SerializeField] private Transform enemyBossTrans;
+
+    public Vector3 GetAllySlotPos(int index, int total) {
+        return LerpByMidpointRule(allyTopTrans.position, allyBottomTrans.position, index, total);
+    }
+
+    public Vector3 GetEnemySlotPos(int index, int total, EnemyLayoutFomation enemyLayoutFomation) {
+        return enemyLayoutFomation switch {
+            EnemyLayoutFomation.Line => LerpByMidpointRule(enemyTopTrans.position, enemyBottomTrans.position, index, total),
+            EnemyLayoutFomation.BossTriangle => GetEnemyBossTrianglePos(index, total),
+            _ => LerpByMidpointRule(enemyTopTrans.position, enemyBottomTrans.position, index, total)
+        };
+    }
+
+
+    private Vector3 GetEnemyBossTrianglePos(int index, int total) {
+        if (index == 0) {
+            return enemyBossTrans.position;
+        }
+        else {
+            return LerpByMidpointRule(enemyTopTrans.position, enemyBottomTrans.position, index, total);
+        }
+    }
+
+
+    #region 线段等分算法
+    private Vector3 LerpByMidpointRule(Vector3 start, Vector3 end, int index, int total) {
+        float t = (index + 0.5f) / total;
+        return Vector3.Lerp(start, end, t);
+    }
+    #endregion
+}
